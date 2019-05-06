a1b0206e3aacd883d3a1b71e762d50bbfab07012
machine <janwa039@gmail.com>
2019-05-06 21:11:58 +0000

    74304

--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.link.js
- * Version: 3.5.0-beta.74224
+ * Version: 3.5.0-beta.74304
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -16773,7 +16773,7 @@ const factoryDefaults = {
    */
 };function factory(plugins, options = factoryDefaults) {
   // Log the SDK's version (templated by webpack) on initialization.
-  let version = '3.5.0-beta.74224';
+  let version = '3.5.0-beta.74304';
   log.info(`CPaaS SDK version: ${version}`);
 
   var sagas = [];
