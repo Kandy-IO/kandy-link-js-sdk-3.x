8c0b07efdf0010a3f36a43d68b2c3b2d6fa04ac6
machine <janwa039@gmail.com>
2019-05-22 22:55:37 +0000

    78010

--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.link.js
- * Version: 3.5.0-beta.77973
+ * Version: 3.5.0-beta.78010
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -16792,7 +16792,7 @@ const factoryDefaults = {
    */
 };function factory(plugins, options = factoryDefaults) {
   // Log the SDK's version (templated by webpack) on initialization.
-  let version = '3.5.0-beta.77973';
+  let version = '3.5.0-beta.78010';
   log.info(`UC SDK version: ${version}`);
 
   var sagas = [];
