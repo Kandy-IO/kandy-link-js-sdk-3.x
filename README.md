c226964f731354cacaf8182b67c9b7516a95392f
machine <janwa039@gmail.com>
2019-10-02 15:30:03 +0000

    148

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ To install a particular build branch or beta branch with the Github URL, run:
 ### In the browser :
 ```html
 <script src="https://unpkg.com/@kandy-io/link-sdk@3.8.0/dist/kandy.js"
-  integrity="sha384-suryjhfK6CxnqS8YMxw/3yxv/rCfpWFuSgNghotU4HbSgbRm6mStVJGry2NEe14e"
+  integrity="sha384-vanHLm9xp4FJDWowUjScg8+BgTEJloKZQVJmwNiEXy8hWxWsyRcOP+mLefzg+E2h"
   crossorigin="anonymous">
 </script>
 ```
--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.link.js
- * Version: 3.8.0-beta.147
+ * Version: 3.8.0-beta.148
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -63075,7 +63075,7 @@ const factoryDefaults = {
    */
 };function factory(plugins, options = factoryDefaults) {
   // Log the SDK's version (templated by webpack) on initialization.
-  let version = '3.8.0-beta.147';
+  let version = '3.8.0-beta.148';
   log.info(`SDK version: ${version}`);
 
   var sagas = [];
