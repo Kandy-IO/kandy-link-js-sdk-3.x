0a96d008abc7950282dc698b124d393924e213cd
machine <janwa039@gmail.com>
2020-08-13 13:15:37 +0000

    498

--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.link.js
- * Version: 3.19.0-beta.497
+ * Version: 3.19.0-beta.498
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -60971,7 +60971,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '3.19.0-beta.497';
+  return '3.19.0-beta.498';
 }
 
 /***/ }),
