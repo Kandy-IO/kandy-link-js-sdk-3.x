876711983b0e136b5a6b766ef0527bcf6772b843
machine <janwa039@gmail.com>
2020-10-29 22:38:13 +0000

    564

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ To install a particular build branch or beta branch with the Github URL, run:
 ### In the browser :
 ```html
 <script src="https://unpkg.com/@kandy-io/link-sdk-3.x@3.21.0/dist/kandy.js"
-  integrity="sha384-phd0SU+PxoDO5N4XSsnnj1/LLA0gvq6K4WPBtJmjukQS0yvofKEwWrcwdTjj4NYM"
+  integrity="sha384-l1u5TyhKnCIaXKBWMUXiRswAKuJOVgufVHiRNhS19BvoGdQUBaElE1Yjobx95a/N"
   crossorigin="anonymous">
 </script>
 ```
--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.link.js
- * Version: 3.21.0-beta.563
+ * Version: 3.21.0-beta.564
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -61026,7 +61026,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '3.21.0-beta.563';
+  return '3.21.0-beta.564';
 }
 
 /***/ }),
