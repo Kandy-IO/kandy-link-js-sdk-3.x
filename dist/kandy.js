49e9a4335227d5b1212694343cbf6c1b867833e0
machine <janwa039@gmail.com>
2021-04-16 11:55:39 +0000

    648

--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.link.js
- * Version: 3.27.0-beta.647
+ * Version: 3.27.0-beta.648
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -61311,7 +61311,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '3.27.0-beta.647';
+  return '3.27.0-beta.648';
 }
 
 /***/ }),
