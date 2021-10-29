b25dec5b339dacadb05863e7cc9067c93f080d3e
machine <janwa039@gmail.com>
2021-10-29 17:23:52 +0000

    781

--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.link.js
- * Version: 3.33.0-beta.780
+ * Version: 3.33.0-beta.781
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -8119,7 +8119,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '3.33.0-beta.780';
+  return '3.33.0-beta.781';
 }
 
 /***/ }),
