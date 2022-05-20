a1d5025cb32f6147254e496265b527699b288c68
machine <janwa039@gmail.com>
2022-05-20 15:23:28 +0000

    884

--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.link.js
- * Version: 3.40.0-beta.883
+ * Version: 3.40.0-beta.884
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -8125,7 +8125,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '3.40.0-beta.883';
+  return '3.40.0-beta.884';
 }
 
 /***/ }),
